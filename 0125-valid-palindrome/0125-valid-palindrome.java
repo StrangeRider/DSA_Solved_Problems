dc257bbc470ec7fdac16f1d6e861c98d4bd65499
Kumar Shaurya <krshaurya05@gmail.com>
2026-08-05 04:01:00 +0530

    Time: 4 ms (46.14%), Space: 44.8 MB (43.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
-        String newStr = "";
+        StringBuilder newStr = new StringBuilder();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             if(Character.isLetterOrDigit(ch)){  //Character.isLetterOrDigit(ch)
-                newStr+=ch;
+                newStr.append(ch);
             }
         }
         int i=0;
